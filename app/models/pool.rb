28b568d1679653baa098b01712cffe1cdd5f518e
hamza bellamine <hamza.bellamine@gmail.com>
2020-04-10 09:43:22 +0100

    done

--- app/models/pool.rb
+++ app/models/pool.rb
@@ -2,6 +2,7 @@ class Pool < ApplicationRecord
   # geocoded_by :address
   # after_validation :geocode, if: :will_save_change_to_address?
   include PgSearch::Model
+
    pg_search_scope :search_by_location_and_option,
     against: [:location, :option],
     using: {
